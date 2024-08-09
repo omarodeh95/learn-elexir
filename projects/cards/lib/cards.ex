2973f4123e6241e4ce580f322a48dd3a14e9a080
Omar Odeh <omar.odeh95@hotmail.com>
2024-08-09 16:21:22 +0300

    Add shuffle funciton

--- projects/cards/lib/cards.ex
+++ projects/cards/lib/cards.ex
@@ -1,7 +1,11 @@
 defmodule Cards do
 
-  def hello do
-    "Hello World!"
+  def create_deck do
+    ["Ace", "Two", "Three"]
   end
-  
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
 end
