6025b7e3324c3137b524118db6d694ec902cdd8b
Omar Odeh <omar.odeh95@hotmail.com>
2024-08-09 15:51:01 +0300

    Test Hello World in iex (interactive)

--- projects/cards/lib/cards.ex
+++ projects/cards/lib/cards.ex
@@ -1,18 +1,7 @@
 defmodule Cards do
-  @moduledoc """
-  Documentation for `Cards`.
-  """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Cards.hello()
-      :world
-
-  """
   def hello do
-    :world
+    "Hello World!"
   end
+  
 end
