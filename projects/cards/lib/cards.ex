eb93735423e3d99e721e4317c54ed5bca3f4c070
Omar Odeh <omar.odeh95@hotmail.com>
2024-08-10 03:09:49 +0300

    Handle no entity error with case statement\

--- projects/cards/lib/cards.ex
+++ projects/cards/lib/cards.ex
@@ -47,7 +47,11 @@ defmodule Cards do
   end
 
   def load(filename) do
-    {_status, binary} = File.read(filename)
-    :erlang.binary_to_term(binary)
+    {status, binary} = File.read(filename)
+
+    case status do
+      :ok -> :erlang.binary_to_term(binary)
+      :error -> "That file does not exist"
+    end
   end
 end
