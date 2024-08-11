5a65610899c3ee42d5c0128ba1dd9851647214e6
Omar Odeh <omar.odeh95@hotmail.com>
2024-08-11 21:07:28 +0300

    Draw image and save it

--- projects/identicon/lib/identicon.ex
+++ projects/identicon/lib/identicon.ex
@@ -6,6 +6,38 @@ defmodule Identicon do
     |> pick_color
     |> build_grid
     |> filter_odd_squares
+    |> build_pixel_map
+    |> draw_image
+    |> save_image(input)
+  end
+
+  def save_image(image, filename) do
+    File.write("#{filename}.png", image)
+  end
+
+  def draw_image(%Identicon.Image{color: color, pixel_map: pixel_map}) do
+    image = :egd.create(250, 250)
+    fill = :egd.color(color)
+
+    Enum.each pixel_map, fn({start, stop}) ->
+      :egd.filledRectangle(image, start, stop, fill)
+    end
+
+    :egd.render(image)
+  end
+
+  def build_pixel_map(%Identicon.Image{grid: grid} = image) do
+    pixel_map = Enum.map grid, fn({_code, index}) ->
+      horizontal = rem(index, 5) * 50
+      vertical = div(index, 5) * 50
+
+      top_left = {horizontal, vertical}
+      top_right = {horizontal + 50, vertical + 50}
+
+      {top_left, top_right}
+    end
+
+    %Identicon.Image{image | pixel_map: pixel_map}
   end
 
   def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
@@ -38,7 +70,7 @@ defmodule Identicon do
 
   def hash_input(input) do
     hex = :crypto.hash(:md5, input)
-    |>:binary.bin_to_list()
+    |> :binary.bin_to_list()
 
     %Identicon.Image{hex: hex}
   end
