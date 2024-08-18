7d6c3dece03cdfb84464ac02fc029923a7478b59
Omar Odeh <omar.odeh95@hotmail.com>
2024-08-19 00:46:59 +0300

    Add topic new route

--- projects/discuss/lib/discuss_web/router.ex
+++ projects/discuss/lib/discuss_web/router.ex
@@ -18,6 +18,8 @@ defmodule DiscussWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :home
+
+    get "/topics/new", TopicsController, :new
   end
 
   # Other scopes may use custom stacks.
