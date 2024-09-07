22a90b7a7eab8a4060f7c2686da2be16e3b82e1f
Omar Odeh <omar.odeh95@hotmail.com>
2024-09-07 05:06:53 +0300

    Create or update user and modify session and redirect

--- projects/discuss/lib/discuss_web/controllers/auth_controller.ex
+++ projects/discuss/lib/discuss_web/controllers/auth_controller.ex
@@ -1,12 +1,34 @@
 defmodule DiscussWeb.AuthController do
   use DiscussWeb, :controller
+  alias Discuss.Repo
+  alias Discuss.User
 
-  def callback(conn, params) do
-    IO.puts("+++++++++++++++++++++++")
-    IO.inspect(conn.assigns)
-    IO.puts("+++++++++++++++++++++++")
-    IO.inspect(params)
-    IO.puts("+++++++++++++++++++++++")
+  def callback(%{assigns: %{ueberauth_auth: auth}} = conn, params) do
+    user_params = %{email: auth.info.email, provider: "github", token: auth.credentials.token}
+    changeset = User.changeset(%User{}, user_params)
+    sign_in(conn, changeset)
+  end
+
+  defp sign_in(conn, changeset) do
+    case create_or_update_user(changeset) do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "Welcome Back!")
+        |> put_session(:user_id, user.id)
+        |> redirect(to: ~p"/topics")
+      {:error, _message} ->
+        conn
+        |> put_flash(:error, "Error signing in")
+        |> redirect(to: ~p"/topics")
+    end
+  end
+
+  defp create_or_update_user(changeset) do
+    case Repo.get_by(User, email: changeset.changes.email) do
+      nil ->
+        {:ok, Repo.insert!(changeset)}
+      user ->
+        {:ok, user}
+    end
   end
 end
-
