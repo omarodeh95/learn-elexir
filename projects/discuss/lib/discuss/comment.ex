d60e1877a9e9c5ed7f18014fe17ffa30ce49dc3b
Omar Odeh <omar.odeh95@hotmail.com>
2024-09-10 04:13:22 +0300

    Create comment schema

---
+++ projects/discuss/lib/discuss/comment.ex
@@ -0,0 +1,23 @@
+defmodule Discuss.Comment do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  alias Discuss.Topic
+  alias Discuss.User
+
+  schema "comments" do
+    field :content, :string
+    belongs_to :topic, Topic
+    belongs_to :user, User
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(comment, attrs) do
+    comment
+    |> cast(attrs, [:content, :user_id, :topic_id])
+    |> validate_required([:content, :user_id, :topic_id])
+  end
+end
+
