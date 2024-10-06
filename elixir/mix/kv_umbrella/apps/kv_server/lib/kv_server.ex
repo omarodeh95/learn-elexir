bfcacf106359f8ebdecd27d935bb513e36b4746a
Omar Odeh <omar.odeh95@hotmail.com>
2024-10-07 01:21:50 +0300

    Clean code using msg

--- elixir/mix/kv_umbrella/apps/kv_server/lib/kv_server.ex
+++ elixir/mix/kv_umbrella/apps/kv_server/lib/kv_server.ex
@@ -29,21 +29,6 @@ defmodule KVServer do
 
   defp serve(socket) do
     msg =
-      case read_line(socket) do
-        {:ok, data} ->
-          case KVServer.Command.parse(data) do
-            {:ok, command} ->
-              KVServer.Command.run(command)
-
-            {:error, _} = err ->
-              err
-          end
-
-        {:error, _} = err ->
-          err
-      end
-
-    msg =
       with {:ok, data} <- read_line(socket),
            {:ok, command} <- KVServer.Command.parse(data),
            do: KVServer.Command.run(command)
