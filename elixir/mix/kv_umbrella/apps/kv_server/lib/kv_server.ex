28dc5079ea882b99e3868a56fe158ba390dd97dd
Omar Odeh <omar.odeh95@hotmail.com>
2024-10-06 19:23:30 +0300

    Add logic for simple server
    The logic added is mainly around listening to a port
    then going through a loop accepting different connection and echoing
    requests as response

--- elixir/mix/kv_umbrella/apps/kv_server/lib/kv_server.ex
+++ elixir/mix/kv_umbrella/apps/kv_server/lib/kv_server.ex
@@ -1,18 +1,38 @@
 defmodule KVServer do
-  @moduledoc """
-  Documentation for `KVServer`.
-  """
+  require Logger
 
-  @doc """
-  Hello world.
+  def accept(port) do
 
-  ## Examples
+    {:ok, socket} =
+      :gen_tcp.listen(port, [:binary, packet: :line, active: false, reuseaddr: true])
 
-      iex> KVServer.hello()
-      :world
+    Logger.info("Accepting connection on port #{port}")
 
-  """
-  def hello do
-    :world
+    loop_acceptor(socket)
+  end
+
+  defp loop_acceptor(socket) do
+    {:ok, client} = :gen_tcp.accept(socket)
+
+    serve(client)
+    loop_acceptor(socket)
+  end
+  
+  defp serve(socket) do
+    socket
+    |> read_line
+    |> write_line(socket)
+
+    serve(socket)
+  end
+
+  defp read_line(socket) do
+    {:ok, data} = :gen_tcp.recv(socket, 0)
+
+    data
+  end
+
+  defp write_line(line, socket) do
+     :gen_tcp.send(socket, line)
   end
 end
