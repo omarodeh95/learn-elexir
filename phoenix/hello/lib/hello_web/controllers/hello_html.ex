cf375fadf7939f66cd3ca88ca985db81f161ab03
Omar Odeh <omar.odeh95@hotmail.com>
2024-10-08 20:59:50 +0300

    Use embedded templates

--- phoenix/hello/lib/hello_web/controllers/hello_html.ex
+++ phoenix/hello/lib/hello_web/controllers/hello_html.ex
@@ -1,9 +1,10 @@
 defmodule HelloWeb.HelloHTML do
   use HelloWeb, :html
 
-  def index(assigns) do
-    ~H"""
-    Hi!
-    """
-  end
+  # def index(assigns) do
+  #   ~H"""
+  #   Hi!
+  #   """
+  # end
+  embed_templates "hello_html/*"
 end
