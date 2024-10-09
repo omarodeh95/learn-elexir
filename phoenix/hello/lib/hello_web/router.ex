02a1bc0f6187d91b35f09853cfab39546b22f859
Omar Odeh <omar.odeh95@hotmail.com>
2024-10-09 06:49:04 +0300

    Add routes for products crud operation

--- phoenix/hello/lib/hello_web/router.ex
+++ phoenix/hello/lib/hello_web/router.ex
@@ -19,6 +19,7 @@ defmodule HelloWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :home
+    resources "/products", ProductController
 
     get "/hello", HelloController, :index
     get "/hello/:messenger", HelloController, :show
