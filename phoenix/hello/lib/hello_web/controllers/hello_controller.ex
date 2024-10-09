5e66df35cbad3b4a851c40a1adcceb87109d1a4c
Omar Odeh <omar.odeh95@hotmail.com>
2024-10-09 06:26:38 +0300

    Test flash message

--- phoenix/hello/lib/hello_web/controllers/hello_controller.ex
+++ phoenix/hello/lib/hello_web/controllers/hello_controller.ex
@@ -2,7 +2,10 @@ defmodule HelloWeb.HelloController do
   use HelloWeb, :controller
 
   def index(conn, _params) do
-    render(conn, :index)
+    conn
+    |> put_flash(:info, "Some success message")
+    |> render(:index)
+    # render(conn, :index)
   end
 
   def show(conn, %{"messenger" => messenger} = _params) do
