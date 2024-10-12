0a7061231e5a7decc550c133466376f2e605d119
Omar Odeh <omar.odeh95@hotmail.com>
2024-10-12 06:02:52 +0300

    Add number validation to quantity

--- phoenix/hello/lib/hello/shopping_cart/cart_item.ex
+++ phoenix/hello/lib/hello/shopping_cart/cart_item.ex
@@ -17,5 +17,6 @@ defmodule Hello.ShoppingCart.CartItem do
     cart_item
     |> cast(attrs, [:price_when_carted, :quantity])
     |> validate_required([:price_when_carted, :quantity])
+    |> validate_number(:quantity, greater_than_or_equal_to: 1, less_than: 100)
   end
 end
