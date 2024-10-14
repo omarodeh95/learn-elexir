ec68cde2e1e7fd42a0c49f16bc70fb3d4d15e543
Omar Odeh <omar.odeh95@hotmail.com>
2024-10-14 03:36:54 +0300

    Modify shopping cart context to serve specific functionalities

--- phoenix/hello/lib/hello/shopping_cart.ex
+++ phoenix/hello/lib/hello/shopping_cart.ex
@@ -4,9 +4,22 @@ defmodule Hello.ShoppingCart do
   """
 
   import Ecto.Query, warn: false
+  alias Hello.Catalog
   alias Hello.Repo
 
-  alias Hello.ShoppingCart.Cart
+  alias Hello.Catalog.Cart
+  alias Hello.ShoppingCart.{Cart, CartItem}
+
+  def get_cart_by_user_uuid(user_uuid) do
+    Repo.one(
+      from c in Cart,
+      where: c.user_uuid == ^user_uuid,
+      left_join: i in assoc(c, :items),
+      left_join: p in assoc(i, :product),
+      order_by: [asc: i.inserted_at],
+      preload: [items: {i, [product: p]}]
+    )
+  end
 
   @doc """
   Returns the list of carts.
@@ -49,10 +62,46 @@ defmodule Hello.ShoppingCart do
       {:error, %Ecto.Changeset{}}
 
   """
-  def create_cart(attrs \\ %{}) do
-    %Cart{}
-    |> Cart.changeset(attrs)
-    |> Repo.insert()
+  # def create_cart(attrs \\ %{}) do
+  #   %Cart{}
+  #   |> Cart.changeset(attrs)
+  #   |> Repo.insert()
+  # end
+
+  def create_cart(user_uuid) do
+    %Cart{user_uuid: user_uuid}
+    |> Cart.changeset(%{})
+    |> Repo.insert
+    |> case do
+      {:ok, cart} -> {:ok, reload_cart(cart)}
+      {:error, changeset} -> {:error, changeset}
+    end
+  end
+
+  def add_item_to_cart(%Cart{} = cart, product_id) do
+    product = Catalog.get_product!(product_id)
+
+    %CartItem{quantity: 1, price_when_carted: product.price}
+    |> CartItem.changeset(%{})
+    |> Ecto.Changeset.put_assoc(:cart, cart)
+    |> Ecto.Changeset.put_assoc(:product, product)
+    |> Repo.insert(
+      on_conflict: [inc: [quantity: 1]],
+      conflict_target: [:cart_id, :product_id]
+    )
+  end
+
+  defp reload_cart(%Cart{} = cart), do: get_cart_by_user_uuid(cart.user_uuid)
+
+  def remove_item_from_cart(%Cart{} = cart, product_id) do
+    {1, _} = 
+      Repo.delete_all(
+        from i in CartItem,
+        where: i.cart_id == ^cart.id,
+        where: i.product_id == ^product_id
+      )
+
+    {:ok, reload_cart(cart)}
   end
 
   @doc """
@@ -68,9 +117,21 @@ defmodule Hello.ShoppingCart do
 
   """
   def update_cart(%Cart{} = cart, attrs) do
-    cart
-    |> Cart.changeset(attrs)
-    |> Repo.update()
+    changeset = 
+      cart
+      |> Cart.changeset(attrs)
+      |> Ecto.Changeset.cast_assoc(:items, with: &CartItem.changeset/2)
+
+    Ecto.Multi.new()
+    |> Ecto.Multi.update(:cart, changeset)
+    |> Ecto.Multi.delete_all(:discarded_items, fn %{cart: cart} -> 
+      from(i in CartItem, where: i.cart_id == ^cart.id and i.quantity == 0)
+    end)
+    |> Repo.transaction()
+    |> case do
+      {:ok, %{cart: cart}} -> {:ok, cart}
+      {:error, :cart, changeset, _changes_so_far} -> {:error, changeset}
+    end
   end
 
   @doc """
@@ -196,5 +257,17 @@ defmodule Hello.ShoppingCart do
   """
   def change_cart_item(%CartItem{} = cart_item, attrs \\ %{}) do
     CartItem.changeset(cart_item, attrs)
+  end
+
+  def total_item_price(%CartItem{} = cart_item) do
+    Decimal.mult(cart_item.product.price, cart_item.quantity)
+  end
+
+  def total_cart_price(%Cart{} = cart) do
+    Enum.reduce(cart.items, 0, fn item, acc ->
+      item
+      |> total_item_price()
+      |> Decimal.add(acc)
+    end)
   end
 end
