e2555a2a43ca08a9ac7c386b00b133a95bc9ca34
Omar Odeh <omar.odeh95@hotmail.com>
2024-10-14 03:37:29 +0300

    Define plugs to load current user and current user cart

--- phoenix/hello/lib/hello_web/router.ex
+++ phoenix/hello/lib/hello_web/router.ex
@@ -9,6 +9,31 @@ defmodule HelloWeb.Router do
     plug :protect_from_forgery
     plug :put_secure_browser_headers
     plug HelloWeb.Plugs.Locale, "en"
+    plug :fetch_current_user
+    plug :fetch_current_cart
+  end
+
+  defp fetch_current_user(conn, _) do
+    if user_uuid = get_session(conn, :current_uuid) do
+      assign(conn, :current_uuid, user_uuid)
+    else
+      new_uuid = Ecto.UUID.generate()
+
+      conn
+      |> assign(:current_uuid, new_uuid)
+      |> put_session(:current_uuid, new_uuid)
+    end
+  end
+
+  alias Hello.ShoppingCart
+  defp fetch_current_cart(conn, _) do
+    if cart = ShoppingCart.get_cart_by_user_uuid(conn.assigns.current_uuid) do
+      assign(conn, :cart, cart)
+    else
+      # create cart
+      {:ok, new_cart} = ShoppingCart.create_cart(conn.assigns.current_uuid)
+      assign(conn, :cart, new_cart)
+    end
   end
 
   pipeline :api do
