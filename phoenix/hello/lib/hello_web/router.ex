e696a5e3c19dade6dc615c892039e981a5714aa4
Omar Odeh <omar.odeh95@hotmail.com>
2024-10-15 04:04:29 +0300

    Define a route for urls resources

--- phoenix/hello/lib/hello_web/router.ex
+++ phoenix/hello/lib/hello_web/router.ex
@@ -25,6 +25,7 @@ defmodule HelloWeb.Router do
     end
   end
 
+  # alias HelloWeb.UrlController
   alias Hello.ShoppingCart
   defp fetch_current_cart(conn, _) do
     if cart = ShoppingCart.get_cart_by_user_uuid(conn.assigns.current_uuid) do
@@ -58,9 +59,11 @@ defmodule HelloWeb.Router do
   end
 
   # Other scopes may use custom stacks.
-  # scope "/api", HelloWeb do
-  #   pipe_through :api
-  # end
+  scope "/api", HelloWeb do
+    pipe_through :api
+
+    resources "/urls", UrlController, except: [:new, :edit]
+  end
 
   # Enable LiveDashboard and Swoosh mailbox preview in development
   if Application.compile_env(:hello, :dev_routes) do
